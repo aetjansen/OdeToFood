225a21235116a77334fc55241bbad2ab72f8cf45
Arno <arno@jansenpoolman.nl>
2020-02-04 13:59:54 +0100

    Add detail view and route to int in url as id

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -9,6 +9,7 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -23,6 +24,11 @@ namespace OdeToFood.Data
                 new Restaurant { Id = 3, Name = "Enormous Escargots", Location = "Zwolle", Cuisine = CuisineType.French },
                 new Restaurant { Id = 4, Name = "Super Stampot", Location = "Groningen", Cuisine = CuisineType.Dutch }
             };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
         }
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
